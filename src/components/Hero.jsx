b7d977ba86cabb0c9e6f8b70ef842600c5e36dc8
olivebishop <olivehendrilgen1@gmail.com>
2023-07-12 02:08:11 +0300

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,16 +13,16 @@ const HeroPage = () => {
 
       {/* Text Section */}
       <div className="w-full lg:w-1/2 p-10">
-        <h1 className="text-4xl font-bold mb-6 mt-20">ITISHA UBEBWE</h1>
-        <p className="text-lg mb-10 font-bold">
-          Call us for your next trip, tunakubeba mizigo yako popte pale
+        <h1 className="text-6xl font-bold mb-6 mt-20">ITISHA UBEBWE</h1>
+        <p className="text-2xl mb-10 font-bold ">
+          Call us for your next trip <br/> tunakubebea mizigo yako popte pale
         </p>
         <div className="mb-6">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+          <button className="bg-purple-500 hover:bg-slate-600 text-white font-semibold py-2 px-4 rounded">
             Download Now
           </button>
           <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 ml-4 rounded">
-            Learn More
+            Get Started
           </button>
         </div>
         <p className="font-bold">
