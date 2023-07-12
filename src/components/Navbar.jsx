5bc894b6197f423ee5c2cf26d15c770a6431c973
olivebishop <olivehendrilgen1@gmail.com>
2023-07-12 18:10:47 +0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState } from 'react';
+import Link from 'next/link';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -18,19 +19,25 @@ const Navbar = () => {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <a
-                  href="#"
+                  href="/"
                   className="text-gray-300 hover:bg-purple-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Home
                 </a>
                 <a
-                  href="#"
+                  href="/about"
                   className="text-gray-300 hover:bg-purple-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   About
                 </a>
                 <a
-                  href="#"
+                  href="/services"
+                  className="text-gray-300 hover:bg-purple-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Services
+                </a>
+                <a
+                  href="contact"
                   className="text-gray-300 hover:bg-purple-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Contact
@@ -42,7 +49,7 @@ const Navbar = () => {
             <button
               onClick={toggleMenu}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
