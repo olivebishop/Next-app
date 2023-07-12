ba6b72dafb4f02cf33fdc32bfde98a753f95cc1b
olivebishop <olivehendrilgen1@gmail.com>
2023-07-12 18:37:51 +0300

    changes in sm navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,24 +87,30 @@ const Navbar = () => {
 
       <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+          <Link href="/">
+          <span  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             Home
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+            </span>
+            </Link>
+          
+          <Link href="/about">
+          <span  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             About
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
+            </span>
+            </Link>
+          
+          <Link href="/services">
+          <span  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Services
+            </span>
+            </Link>
+          
+          <Link href="/contact">
+          <span  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             Contact
-          </a>
+            </span>
+            </Link>
+          
         </div>
       </div>
     </nav>
